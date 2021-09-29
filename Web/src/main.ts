8af16239c312e93dcee063b5c923034519ae6a15
Ever2after <62290044+Ever2after@users.noreply.github.com>
2021-09-29 17:26:42 +0900

    Revert "기본적인 페이지 구조 생성"

--- Web/src/main.ts
+++ Web/src/main.ts
@@ -1,11 +1,8 @@
 import Vue from "vue";
-import App from "@/App.vue";
-import router from "@/router";
-import store from "@/store";
-import vuetify from "@/plugins/vuetify";
-import "@/plugins/class-component";
-
-import "@/styles/main.scss";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 
