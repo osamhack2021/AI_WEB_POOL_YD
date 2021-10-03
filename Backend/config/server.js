02618e981f791a885edd2613c590ccea1c9b663b
ever2after <dhwntkd412@sasa.hs.kr>
2021-10-03 08:12:53 +0000

    Set the basic db models
    Setted Post, Comment, User db models.

--- Backend/config/server.js
+++ Backend/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 80),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '8cea754f8cfb907717ed2c72218520e5'),
