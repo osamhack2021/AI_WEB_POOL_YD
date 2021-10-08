9582b51a487ef56f66cc41b1bd8a9506c36c7aa8
ever2after <dhwntkd412@sasa.hs.kr>
2021-10-08 10:16:15 +0000

    Update db models
    User, Post, Comment 모델을 notion에 언급된 명세서 대로 업데이트합니다.
    그리고 post 모델에 대해 기본적인 customed api를 구현합니다.
    port를 머신 환경에 맞추어 1337 -> 443 으로 변경합니다.

--- Backend/api/post/controllers/post.js
+++ Backend/api/post/controllers/post.js
@@ -1,8 +1,28 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+const { sanitizeEntity } = require('strapi-utils');
 
-module.exports = {};
+module.exports = {
+    async full(ctx) {
+        const _posts = await strapi.query("post").find();
+        const results = await Promise.all(_posts.map(async _post => {
+            let commentIds = _post.comments.map(_co => { return _co._id });
+            return {
+                ..._post,
+                comments: await strapi.query("comment").find({id_in: commentIds}),
+            };
+        }));
+        return { data : results };
+    },
+    async preview(ctx) {
+        const _posts = await strapi.query("post").find();
+        const _preview = _posts.map(el => { 
+            return {
+                author: el.author.username,
+                title: el.title,
+                content: el.content, 
+            };
+        });
+        return { data : _preview };
+    },
+};
