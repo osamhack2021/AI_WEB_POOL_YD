64be387ffe4c78a100e5fba0649f3c8a2d053c8e
ever2after <dhwntkd412@sasa.hs.kr>
2021-10-18 11:17:47 +0000

    Make 'feed recommendation' api
    피드 추천 api를 구현합니다

--- Backend/api/post/controllers/post.js
+++ Backend/api/post/controllers/post.js
@@ -283,110 +283,203 @@ module.exports = {
     },
     async discover(ctx) {
         const { body } = ctx.request;
-        const { target, tags, number } = body;
-        let filter1 = [];
-        let filter2 = [];
-        let filter3 = [];
-        let users;
-        let posts;
-        switch(target) {
-            case 'user':
-                tags.forEach(tag => {
-                    if (tag.subject === "계급") filter1.push(tag.tag);
-                    else if (tag.subject === "병과") filter2.push(tag.tag);
-                    else filter3.push(tag.tag);
-                });
-                users = await strapi.query("user", "users-permissions").find();
-                users = users.filter(user => {
-                    if(user.soldierData) {
-                        let e = false;
-                        user.tags.forEach(tag => {
-                            if (filter3.indexOf(tag.content) > -1) {
-                                e = true;
-                            };
-                        });
-                        return (filter1.indexOf(user.soldierData.rank) > -1 || filter1.length === 0)
-                        && (filter2.indexOf(user.soldierData.militaryBranch) > -1 || filter2.length === 0)
-                        && (e || filter3.length === 0);
-                    }
-                    return false;
-                });
-                users = await Promise.all(users.map(async user => {
-                    return {
-                        id: user.id,
-                        username: user.username,
-                        birthday: user.birthday,
-                        telno: user.telno,
-                        location: user.location,
-                        email: user.email,
-                        summary: user.summary,
-                        department: user.soldierData.department,
-                        militaryBranch: user.soldierData.militaryBranch,
-                        role: user.soldierData.role,
-                        profileImageUrl: user.thumbnail.url,
-                        postCount: user.posts.length,
-                        followersCount: user.followers.length
-                    }
-                }));
-                return { data: users } 
-            case 'pool':
-                break;
-            case 'recruition':
-                tags.forEach(tag => {
-                    if (tag.subject === "계급 조건") filter1.push(tag.tag);
-                    else if (tag.subject === "관련 병과") filter2.push(tag.tag);
-                    else filter3.push(tag.tag);
-                });
-                posts = await strapi.query("post").find();
-                posts = posts.filter(post => post.postType === "recruition");
-                filter1 = await Promise.all(filter1.map(async el => {
-                    switch(el){
-                        case "계급 무관": 
-                            return "private";
-                        case "하사 이상":
-                            return "sergeant";
-                        case "소위 이상":
-                            return "lieutenant";
-                        case "소령 이상":
-                            return "major";
-                        case "준장 이상":
-                            return "general";
-                    }
-                }));
-                const isTarget = await Promise.all(posts.map(async post => {
-                    let e = false;
-                    let relatedBranches = await strapi.query("tag").find({id_in: post.jobInfo.relatedBranches});
-                    relatedBranches.forEach(tag => {
-                        if (filter2.indexOf(tag.content) > -1) {
-                            e = true;
-                        }
+        const { type } = body;
+        if (type === "discover") {
+            const { target, tags } = body;
+            let filter1 = [];
+            let filter2 = [];
+            let filter3 = [];
+            let users;
+            let posts;
+            switch(target) {
+                case 'user':
+                    tags.forEach(tag => {
+                        if (tag.subject === "계급") filter1.push(tag.tag);
+                        else if (tag.subject === "병과") filter2.push(tag.tag);
+                        else filter3.push(tag.tag);
                     });
-                    let group = await strapi.query("tag").findOne({id: post.jobInfo.group});
-                    const result = (filter1.indexOf(post.jobInfo.minRank) > -1 || filter1.length === 0)
-                        && (e || filter2.length === 0)
-                        && (filter3.indexOf(group.content) > -1 || filter3.length === 0);
-                    return result;
-                }));
-                posts = posts.filter((post, idx) => {
-                    return isTarget[idx];
-                })
-                posts = await Promise.all(posts.map(async post => {
-                    let _author = await strapi.query("user", "users-permissions").findOne({id: post.author.id});
-                    let url = _author.thumbnail.url;  
-                    return {
-                        id: post.id,
-                        author: {
-                            id: post.author.id,
-                            username: post.author.username,
-                            profileImageUrl: url,
-                        },
-                        title: post.title,
-                        employmentType: post.jobInfo.employmentType,
-                        pay: post.jobInfo.pay,
-                        due: post.jobInfo.due
+                    users = await strapi.query("user", "users-permissions").find();
+                    users = users.filter(user => {
+                        if(user.soldierData) {
+                            let e = false;
+                            user.tags.forEach(tag => {
+                                if (filter3.indexOf(tag.content) > -1) {
+                                    e = true;
+                                };
+                            });
+                            return (filter1.indexOf(user.soldierData.rank) > -1 || filter1.length === 0)
+                            && (filter2.indexOf(user.soldierData.militaryBranch) > -1 || filter2.length === 0)
+                            && (e || filter3.length === 0);
+                        }
+                        return false;
+                    });
+                    users = await Promise.all(users.map(async user => {
+                        return {
+                            id: user.id,
+                            username: user.username,
+                            birthday: user.birthday,
+                            telno: user.telno,
+                            location: user.location,
+                            email: user.email,
+                            summary: user.summary,
+                            department: user.soldierData.department,
+                            militaryBranch: user.soldierData.militaryBranch,
+                            role: user.soldierData.role,
+                            profileImageUrl: user.thumbnail.url,
+                            postCount: user.posts.length,
+                            followersCount: user.followers.length
+                        }
+                    }));
+                    return { data: users } 
+                case 'pool':
+                    break;
+                case 'recruition':
+                    tags.forEach(tag => {
+                        if (tag.subject === "계급 조건") filter1.push(tag.tag);
+                        else if (tag.subject === "관련 병과") filter2.push(tag.tag);
+                        else filter3.push(tag.tag);
+                    });
+                    posts = await strapi.query("post").find();
+                    posts = posts.filter(post => post.postType === "recruition");
+                    filter1 = await Promise.all(filter1.map(async el => {
+                        switch(el){
+                            case "계급 무관": 
+                                return "private";
+                            case "하사 이상":
+                                return "sergeant";
+                            case "소위 이상":
+                                return "lieutenant";
+                            case "소령 이상":
+                                return "major";
+                            case "준장 이상":
+                                return "general";
+                        }
+                    }));
+                    const isTarget = await Promise.all(posts.map(async post => {
+                        let e = false;
+                        let relatedBranches = await strapi.query("tag").find({id_in: post.jobInfo.relatedBranches});
+                        relatedBranches.forEach(tag => {
+                            if (filter2.indexOf(tag.content) > -1) {
+                                e = true;
+                            }
+                        });
+                        let group = await strapi.query("tag").findOne({id: post.jobInfo.group});
+                        const result = (filter1.indexOf(post.jobInfo.minRank) > -1 || filter1.length === 0)
+                            && (e || filter2.length === 0)
+                            && (filter3.indexOf(group.content) > -1 || filter3.length === 0);
+                        return result;
+                    }));
+                    posts = posts.filter((post, idx) => {
+                        return isTarget[idx];
+                    });
+                    posts = await Promise.all(posts.map(async post => {
+                        let _author = await strapi.query("user", "users-permissions").findOne({id: post.author.id});
+                        let url = _author.thumbnail.url;  
+                        return {
+                            id: post.id,
+                            author: {
+                                id: post.author.id,
+                                username: post.author.username,
+                                profileImageUrl: url,
+                            },
+                            title: post.title,
+                            employmentType: post.jobInfo.employmentType,
+                            pay: post.jobInfo.pay,
+                            due: post.jobInfo.due
+                        }
+                    }));
+                    return { data : posts };
+            }
+        } else if (type === "recommend") {
+            const { userID } = body;
+            const user = await strapi.query("user", "users-permissions").findOne({id: userID});
+            let followingPosts = [];
+            await Promise.all(user.followings.map(async following => {
+                const _following = await strapi.query("user", "users-permissions").findOne({id: following.id});
+                const posts = _following.posts.filter(post => post.postType !== "pool");
+                followingPosts = followingPosts.concat(posts);
+                console.log(followingPosts.length);
+                return 0;
+            }));
+            let poolPosts = [];
+            await Promise.all(user.pools.map(async pool => {
+                const targetPool = await strapi.query("pool").findOne({id: pool.id});
+                poolPosts = poolPosts.concat(targetPool.posts);
+            }));
+            let feedPosts = followingPosts.concat(poolPosts);
+            const userEmbedding = user.embedding ? user.embedding.data : null;
+            const postEmbeddingList = feedPosts.map(feed => feed.embedding.data);
+            const _request = {
+                request: "rankRelevantPosts",
+                rankRelevantPostsData: {
+                    userEmbedding,
+                    postEmbeddingList
+                }
+            }
+            const options = {
+                uri: "http://852abae4-3199-4066-9ef5-5132e347d13a.koreacentral.azurecontainer.io/score",
+                method: "POST",
+                body: _request,
+                json: true
+            };
+            const data = await getResult(options);
+            const { index } = data;
+            feedPosts = feedPosts.map((feed, idx) => feedPosts[index[idx]]);
+            feedPosts = await Promise.all(feedPosts.map(async _el => {
+                const el = await strapi.query("post").findOne({id: _el.id});
+                let relatedBranches;
+                let group;
+                let minRank;
+                if(el.postType === "recruition"){
+                    relatedBranches = await strapi.query("tag").find({id_in : el.jobInfo.relatedBranches});
+                    relatedBranches = relatedBranches.map(el1 => el1.content);
+                    group = await strapi.query("tag").findOne({id: el.jobInfo.group});
+                    group = group.content;
+                    switch(el.jobInfo.minRank){
+                        case "private": 
+                            minRank = "계급 무관";
+                            break;
+                        case "sergeant":
+                            minRank = "하사 이상";
+                            break;
+                        case "lieutenant":
+                            minRank = "소위 이상";
+                            break;
+                        case "major":
+                            minRank = "소령 이상";
+                            break;
+                        case "general":
+                            minRank = "준장 이상";
+                            break;
                     }
-                }))
-                return { data : posts };
+                }
+                return {
+                    id: el.id,
+                    postType: el.postType,
+                    createdAt: el.createdAt,
+                    author: {
+                        id: el.author.id,
+                        username: el.author.username,
+                        profileImageUrl: el.author.thumbnail.url,
+                    },
+                    title: el.title,
+                    contentPreview: el.content ? el.content.replace(/<[^>]*>?/gm, '').slice(0,240) + "..." : null,
+                    previewMainImageUrl: el.images.length>0 ? el.images[0].url : null,
+                    tags: el.tags.map(_tag => _tag.content),
+                    commentsCount: el.comments.length,
+                    likesCount: el.likes.length,
+                    jobInfo: el.jobInfo ? {
+                        id: el.jobInfo.id,
+                        desc: el.jobInfo.desc,
+                        employmentType: el.jobInfo.employmentType,
+                        due: el.jobInfo.due,
+                        minRank,
+                        relatedBranches,
+                        group
+                    } : null
+                };
+            }));
+            return feedPosts;
         }
     },
     async test(ctx) {
@@ -549,5 +642,5 @@ module.exports = {
             
             return "done";
         }
-    }
+    },
 };
