e100633010bd6b5eb09f13b3559e79d077b2e873
woosukji <woosukji@snu.ac.kr>
2021-10-19 11:32:21 +0000

    Init resume writer page

---
+++ Web/src/pages/account/ResumeWriter.vue
@@ -0,0 +1,17 @@
+<template>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { AxiosResponse } from "axios";
+
+@Component({
+  components: { },
+})
+export default class MyPage extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
